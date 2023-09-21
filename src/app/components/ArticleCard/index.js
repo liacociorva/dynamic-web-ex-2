09bae4e90f830aae0f28eb4a247e5d56bad5ea01
Lia <lia.cociorva@gmail.com>
2023-09-21 09:43:40 -0400

    articles finishing up

--- src/app/components/ArticleCard/index.js
+++ src/app/components/ArticleCard/index.js
@@ -1,11 +1,19 @@
 "use client";
 
-const ArticleCard = ({description, title}) => {
+import styles from "./articleCard.module.css";
+const ArticleCard = ({description, title, date, imageAlt, imageSrc, link}) => {
     
     return (
-        <div>
+        <div className={styles.articleCard}>
+            <div className={styles.articleImage}>
+                <img src={imageSrc} alt={imageAlt}/>
+            </div>
+            <div className={styles.articleContent}>
             <h2>{title}</h2>
+            <p>{date}</p>
             <p>{description}</p>
+            <p>{link}</p>
+            </div>
         </div>
     );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,28 @@
 "use client";
-import styles from '../app/page.module.css'
-import Data from "../app/components/data"
+
+import Data from "../app/components/data";
+import styles from "../app/page.module.css";
 import ArticleCard from '../app/components/ArticleCard';
 
 export default function Home() {
-  const projectName = "Exercise Two";
+  const projectName = "I Love Articles!!";
   
 
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
+    
         <h1>{projectName}</h1>
-        <ArticleCard title="Cool Title"/>
-        <ArticleCard title="Another Cool Title" description="Cool description"/>
-      </div>
+        {Data.map ((article) => (
+        <ArticleCard 
+        key={article.id}
+        title={article.title} 
+        description = {article.blurb}
+        date = {new Date (article.publishedDate).toDateString()} 
+        imageSrc = {article.image.url} 
+        imageAlt={article.image.alt} 
+        link='Learn more'
+        />
+      ))}
     </main>
   )
 }
